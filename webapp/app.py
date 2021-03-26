ec876c701514d82a74fcfc229e06fd6079115b36
Joseph Song <joesong@microsoft.com>
2021-03-25 23:31:01 -0500

    Sanitizing input from byte

--- webapp/app.py
+++ webapp/app.py
@@ -1,16 +1,22 @@
 import requests
+import ast
 from flask import Flask, render_template, request
+import pandas as pd
+import numpy as np
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route('/')
+def index():
+    return 'hello world'
 
 @app.route("/ticker/", methods = ['POST'])
 def ticker():
     ticker = request.form.get('Ticker')
     url = f"https://mvp-stockpredictor.azurewebsites.net/api/get_stock_price?ticker={ticker}&code=WvXjCoODgLc2HAWd8tF/rt/7hs/6Quqk5aXX2qRtlgSUmi3iNkyFWQ=="
     content = requests.get(url).content
+    content = content.decode('utf-8')
+    content = ast.literal_eval(content)
     print(content)
     print(url)
     return render_template('data.html', content = content)
